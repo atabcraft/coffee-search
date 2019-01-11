f62f6bb461b1caf2549e6ce7a4754cc44e109848
Armin <armin.tiric@fer.hr>
2019-01-11 11:41:50 +0100

    feat: Elasticsearch is now being started as embedded bean of Spring,this approach is used just for testing purpose

--- coffee-service/src/main/java/com/example/search/coffee/CoffeeApplication.java
+++ coffee-service/src/main/java/com/example/search/coffee/CoffeeApplication.java
@@ -1,15 +1,27 @@
 package com.example.search.coffee;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ElasticsearchCrudRepository.class))
+@EnableElasticsearchRepositories(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ElasticsearchCrudRepository.class))
 public class CoffeeApplication {
+    
+        private static final Logger log = LoggerFactory.getLogger(CoffeeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeApplication.class, args);
+                log.info("App started!");
 	}
 
 }
---
+++ coffee-service/src/main/java/com/example/search/coffee/config/ElasticsearchConfig.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.search.coffee.config;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import static java.util.Arrays.asList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.InternalSettingsPreparer;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeValidationException;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.Netty4Plugin;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.scheduling.annotation.Async;
+
+@Configuration
+public class ElasticsearchConfig implements DisposableBean {
+
+    private Node node;
+
+   
+    private static class MyNode extends Node {
+    public MyNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
+        super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null), classpathPlugins);
+       }
+    }
+    
+    
+    public Settings elasticsearchSettings(){
+        return Settings.builder()
+                .put("transport.type", "netty4")
+                .put("client.transport.sniff", true)
+                .put("http.type", "netty4")
+                .put("http.enabled", "true")
+                .put("path.home", "target/elasticsearch")
+                .build();
+    }
+ 
+    @Bean
+    public Client client() {
+
+        Settings settings = elasticsearchSettings();
+        try {
+            
+            TransportClient client = new PreBuiltTransportClient(elasticsearchSettings());
+            client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+            return client;
+
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ElasticsearchConfig.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                    
+        return null;
+    }
+ 
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(
+                
+                client());
+    }
+    
+    
+    @Bean
+    @Async
+    public Node elasticSearchTestNode() throws NodeValidationException {
+        Node node = new MyNode(
+                elasticsearchSettings(),
+                asList(Netty4Plugin.class));
+        this.node = node; 
+        node.start();
+        return node;
+    }
+    
+    @Override
+    public void destroy() throws Exception {
+        node.close();
+    }
+    
+    
+}
